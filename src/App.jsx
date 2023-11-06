591cff8519820d9b117ce4fc7fb2fd85d13ccdb8
pserra <pserra@customia.com>
2023-11-06 08:44:55 +0100

    dia 1

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import CrossIcon from "./components/icons/CrossIcon";
+import MoonIcon from "./components/icons/MoonIcon";
+
+const App = () => {
+    return (
+        <div className="min-h-screen bg-gray-100 bg-[url('/src/assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
+            <header className="container mx-auto px-4 pt-8">
+                <div className="flex justify-between">
+                    <h1 className="text-3xl font-semibold uppercase tracking-[0.3em] text-white">
+                        Todo
+                    </h1>
+                    <button>
+                        <MoonIcon fill="#000" className="fill-red-800" />
+                    </button>
+                </div>
+                <form className="mt-8 flex items-center gap-4 overflow-hidden rounded-md bg-white px-4 py-4">
+                    <span className="inline-block h-5 w-5 rounded-full border-2"></span>
+                    <input
+                        className="w-full text-gray-400 outline-none"
+                        type="text"
+                        placeholder="Create a new todo..."
+                    />
+                </form>
+            </header>
+
+            <main className="container mx-auto mt-8 px-4">
+                <div className="rounded-md bg-white [&>article]:p-4">
+                    <article className="flex gap-4 border-b border-b-gray-300">
+                        <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+                        <p className="grow text-gray-600">
+                            Complete online JavaScript course
+                        </p>
+                        <button className="flex-none">
+                            <CrossIcon />
+                        </button>
+                    </article>
+
+                    <article className="flex gap-4 border-b border-b-gray-300">
+                        <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+                        <p className="grow text-gray-600">
+                            Complete online JavaScript course
+                        </p>
+                        <button className="flex-none">
+                            <CrossIcon />
+                        </button>
+                    </article>
+
+                    <article className="flex gap-4 border-b border-b-gray-300">
+                        <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+                        <p className="grow text-gray-600">
+                            Complete online JavaScript course
+                        </p>
+                        <button className="flex-none">
+                            <CrossIcon />
+                        </button>
+                    </article>
+
+                    <section className="flex justify-between p-4">
+                        <span className="text-gray-400">5 items left</span>
+                        <button className="text-gray-400">
+                            Clear completed
+                        </button>
+                    </section>
+                </div>
+            </main>
+
+            <section className="container mx-auto mt-8 px-4">
+                <div className="flex justify-center gap-4 bg-white p-4">
+                    <button className="text-blue-500">All</button>
+                    <button className="hover:text-blue-500">Active</button>
+                    <button className="hover:text-blue-500">Completed</button>
+                </div>
+            </section>
+
+            <p className="mt-8 text-center">Drag and Drop</p>
+        </div>
+    );
+};
+
+export default App;
